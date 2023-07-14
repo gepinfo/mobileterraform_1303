1e1277ae95f83d2c9b38b2a46b151b377512e9d7
gepinfo <gepinfo>
2023-07-14 14:23:55 +0000

    updated generated code

--- application/services/custom_services/mobilegen/src/seed.ts
+++ application/services/custom_services/mobilegen/src/seed.ts
@@ -1,5 +1,7 @@
 import * as mongoose from 'mongoose';
 import { usersSchema } from './models/daomodels/users';
+import * as mongoose from 'mongoose';
+import { usersSchema } from './models/daomodels/users';
 
 const usersModel = mongoose.model('users', usersSchema);
 
